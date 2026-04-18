4d8530a0107c147cce66e62d07d4d2ddf973a1d5
ds6161855 <DSMEWADA61@GMAIL.COM>
2026-04-18 17:22:49 +0530

    done

--- src/main/java/com/easybuy/EasybuyApplication.java
+++ src/main/java/com/easybuy/EasybuyApplication.java
@@ -2,12 +2,15 @@ package com.easybuy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.easybuy.repository")
+@EntityScan(basePackages = "com.easybuy.entity")
 public class EasybuyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EasybuyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EasybuyApplication.class, args);
+    }
 }
