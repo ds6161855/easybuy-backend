5af84b4c7935bedc03e1628e8efeec0a7da6e1ce
ds6161855 <DSMEWADA61@GMAIL.COM>
2026-07-17 08:14:43 +0530

    done

--- src/main/java/com/easybuy/repository/ProductRepository.java
+++ src/main/java/com/easybuy/repository/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.easybuy.repository;
 
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.easybuy.entity.Product;
@@ -10,12 +12,14 @@ import com.easybuy.entity.Product;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByCategoryIgnoreCase(String category);
+
     List<Product> findByNameContainingIgnoreCaseOrCategoryContainingIgnoreCaseOrBrandContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
-    	    String name,
-    	    String category,
-    	    String brand,
-    	    String description
-    	);
+            String name,
+            String category,
+            String brand,
+            String description
+    );
+
     @Query("SELECT p.name, COUNT(p) FROM Product p GROUP BY p.name HAVING COUNT(p) > 1")
-List<Object[]> findDuplicateProducts();
+    List<Object[]> findDuplicateProducts();
 }
